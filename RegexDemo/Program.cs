298a69a1e97e5aa80abe4f2e9e90fbb08747eb8d
devashree0900 <85434516+devashree0900@users.noreply.github.com>
2023-01-25 22:25:36 +0530

    Update Program.cs

--- RegexDemo/Program.cs
+++ RegexDemo/Program.cs
@@ -8,12 +8,20 @@ namespace RegexDemo
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to regex");
-            User user1 = new User();
-            user1.TakeFirstName();
-            user1.TakeLastName();
-            user1.TakePhone();
-            user1.TakeEmail();
-            user1.TakePassword();
+            //User user1 = new User();
+            //user1.TakeFirstName();
+            //user1.TakeLastName();
+            //user1.TakePhone();
+            //user1.TakeEmail();
+            //user1.TakePassword();
+
+            UserFuncWithException user = new UserFuncWithException();
+            user.TakeFirstNameWithException();
+            user.TakeLastNameWithException();
+            user.TakeEmailWithException();
+            user.TakePhoneWithException();
+            user.TakePasswordWithException();
+
 
 
         }

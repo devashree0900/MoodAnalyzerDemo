aed208adda453bd47efe2dd4227c4041990d47f3
devashree0900 <85434516+devashree0900@users.noreply.github.com>
2023-01-25 22:24:47 +0530

    Add files via upload

---
+++ RegexDemo/UserRegistrationCustomException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class UserRegistrationCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_FNAME,
+            INVALID_LNAME,
+            INVALID_EMAIL,
+            INVALID_PASSWORD,
+            INVALID_PHONE
+        }
+
+        public readonly ExceptionType Type;
+
+        public UserRegistrationCustomException(ExceptionType type, string message) : base(message)
+        {
+            this.Type = type;
+        }
+    }
+}
